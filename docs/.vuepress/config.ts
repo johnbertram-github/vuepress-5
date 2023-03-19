7448c79437e7caa9a9fa583f8856dd0156919791
John Bertram <johnbertram.uk@hotmail.com>
2023-03-19 19:09:37 +0000

    redeploy

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,8 +1,14 @@
-import { defineUserConfig } from 'vuepress'
+import { defineUserConfig, defaultTheme } from 'vuepress'
 
 export default defineUserConfig({
   lang: 'en-GB',
   title: 'Hello VuePress',
   description: 'Just playing around',
   base: '/vuepress-5/',
+  theme: defaultTheme({
+    lastUpdated: true,
+    contributors: true,
+    home: '/',
+    navbar: [{ text: 'Home', link: '/' }],
+  }),
 })
